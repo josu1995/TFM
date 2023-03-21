3c0d7ec179ebfa2921d64aa4d6599c83fd2afa90
josu <josu@citystock.es>
2023-03-21 12:10:28 +0100

    traducciones

--- resources/lang/en/messagesIndex.php
+++ resources/lang/en/messagesIndex.php
@@ -3,22 +3,22 @@
 //resources/lang/en/messages.php
 
 return [
-    'iniciar' => 'Iniciar sesión',
-    'registrar' => 'Registrarse',
-    'titulo' => 'APRENDE IDIOMAS FÁCILMENTE',
-    'variedad' => 'Variedad de idiomas a tu alcance',
-    'probar' => 'Pruébalo gratis',
-    'aprender' => 'Aprende diferentes idiomas',
-    'aprender1'=> 'Descubre la variedad de idiomas y recursos que ofrecemos, para un aprendizaje más completo.',
-    'metodo' => 'Método de estudio diferente',
-    'metodo1' => 'Nuestra web ofrece una metodología novedosa, que ayudarána que aprendas de formas mas eficiente.',
-    'gratis' => 'Totalmente gratis',
-    'gratis1' => 'Podrás estudiar todos los idiomas y dificultades que quieras,sin ningún tipo de coste.',
-    'alta' => ' Date de alta en IdioGrabber totalmente gratis',
-    'empezar' => 'EMPEZAR',
-    'condiciones' => 'Condiciones de uso',
-    'politica' => 'Política de privacidad y cookies',
-    'cookie' => 'IdioGrabber utiliza cookies para mejorar la experiencia de usuario y sus servicios. Si continúa navegando, consideramos que acepta su uso. Más información aquí',
-    'cookie1' => 'Política de Privacidad y Cookies'
+    'iniciar' => 'Login',
+    'registrar' => 'Register',
+    'titulo' => 'LEARN LANGUAGES EASILY',
+    'variedad' => 'Variety of languages ​​at your fingertips',
+    'probar' => 'Try it free',
+    'aprender' => 'Learn different languages',
+    'aprender1'=> 'Discover the variety of languages ​​and resources we offer, for a more complete learning.',
+    'metodo' => 'Different study method',
+    'metodo1' => 'Our website offers a novel methodology that will help you learn more efficiently.',
+    'gratis' => 'Totally free',
+    'gratis1' => 'You can study all the languages ​​and difficulties you want, without any cost.',
+    'alta' => ' Sign up for IdioGrabber for free',
+    'empezar' => 'START',
+    'condiciones' => 'Terms of use',
+    'politica' => 'Privacy and cookies policy',
+    'cookie' => 'IdioGrabber uses cookies to improve the user experience and its services. If you go on surfing, we will consider you accepting its use. More information here',
+    'cookie1' => 'Privacy and Cookies Policy'
 
 ];
