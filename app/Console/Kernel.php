822390c9b5c58919083d6f6a71e18ca5eabc3c22
josu <josu@citystock.es>
2023-03-24 17:15:27 +0100

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,16 +19,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Inspire::class,
-        Commands\EmailFacturas::class,
-        Commands\EmailAlertas::class,
-        Commands\CancelarViaje::class,
-        Commands\AvisoViaje::class,
-        Commands\AvisoDestinatario::class,
-        Commands\DevolverEnvio::class,
-        Commands\TrazasEstadoTracking::class,
-        Commands\FacturasBusiness::class,
-        Commands\TrackingBusiness::class,
+       
+    
     ];
 
     /**
@@ -40,28 +32,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        // Schedule para alertas
-        $schedule->command('email:alertas')->dailyAt('00:00');
-
-        // Schedule para facturas
-        $schedule->command('email:facturas')->monthlyOn(1, '04:00');
-
-        // Schedule para cancelación de viajes
-        $schedule->command('viaje:cancelar')->dailyAt('21:30');
-
-        // Schedule para aviso a transportistas
-        $schedule->command('viaje:aviso')->dailyAt('22:00');
-
-        // Schedule para aviso a destinatarios
-        $schedule->command('destinatario:aviso')->dailyAt('22:00');
-
-        // Schedule para devoluciones de envios
-        $schedule->command('envio:devolver')->dailyAt('22:00');
-
-        // Trazas estado tracking MondialRelay
-        $schedule->command('tracking:business')->everyThirtyMinutes();
-
-        // Generacion de facturas Business
-        $schedule->command('facturas:business')->monthlyOn(1, '01:00');
+       
     }
 }
