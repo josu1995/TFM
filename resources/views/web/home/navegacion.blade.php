b49efaa6a7670acf239b8b8ce69a638f2f919322
josu <josu@citystock.es>
2023-03-24 19:38:27 +0100

    fix

--- resources/views/web/home/navegacion.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container" style="height: 60px; overflow-y: hidden; overflow-x: auto;">
-    <ul class="nav nav-pills nav-fill w-100 align-items-start menu--interior" role="tablist" style="display: -webkit-inline-box; width: 670px;">
-        <li><a href="{{ route('inicio') }}"><i class="fas fa-btn"></i>Panel de usuario</a></li>
-        <li><a href="{{ route('envios') }}"><i class="fas fa-btn"></i>Envíos</a></li>
-        <li><a href="{{ route('mensajes') }}"><i class="fas fa-btn"></i>Notificaciones</a></li>
-        <li><a href="{{ route('perfil_usuario') }}"><i class="fas fa-btn"></i>Perfil</a></li>
-        <li><a href="{{ route('cuenta_usuario') }}"><i class="fas fa-btn"></i>Cuenta</a></li>
-    </ul>
-</div>
--- resources/views/web/partials/cabecera-enviar.blade.php
+++
@@ -1,4 +0,0 @@
-<?php $url = $_SERVER["REQUEST_URI"];?>
-<section class="hero hero-corto {{ isset($class) ? $class : '' }}" id="hero">
-    
-</section>
